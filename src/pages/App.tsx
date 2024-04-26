cfb330d380ec94b0a56c44f572407aa93fde4c37
Sumit.Meena <sumit.meena@xelion.com>
2024-04-26 20:14:33 +0530

    correcting banner

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,9 +5,6 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
     useEffect(() => {
-        console.log(
-            "Hey there, curious mind!\n\nEver wondered what makes things tick? Take a journey into the heart of innovation. Explore the intricate web of code that powers my portfolio: it's all right here, waiting for you to unravel its secrets.\n\nDive into the depths of creativity at: https://github.com/disteroby/disteroby-dev-portfolio.\n\nHappy exploring!",
-        );
     }, []);
 
     return (
